f7fe0ad570d0411b6082f306858a7b79000fee35
Salem Khaled <salemkhaled2060@gmail.com>
2021-08-01 08:44:59 +0200

    add 2 files

---
+++ index.js
@@ -0,0 +1,17 @@
+obj = [
+    {
+        "Id": "1",
+        "FirstName": "Salem",
+        "LastName": "Khaled"
+    },
+    {
+        "Id": "2",
+        "FirstName": "Abdo",
+        "LastName": "Khaled"
+    },
+    {
+        "Id": "3",
+        "FirstName": "Mohaned",
+        "LastName": "Khaled"
+    },
+]
